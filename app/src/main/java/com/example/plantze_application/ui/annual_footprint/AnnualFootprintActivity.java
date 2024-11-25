d83bf071ca3c0dec1dffb22a32902c289040d151
andyW47 <andywang3689@gmail.com>
2024-11-25 14:41:42 -0500

    changed activity_ecotracker xml file name ot activity_annualfootprint

--- app/src/main/java/com/example/plantze_application/ui/annual_footprint/AnnualFootprintActivity.java
+++ app/src/main/java/com/example/plantze_application/ui/annual_footprint/AnnualFootprintActivity.java
@@ -19,7 +19,7 @@ public class AnnualFootprintActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ecotracker);
+        setContentView(R.layout.activity_annualfootprint);
 
         // Initialize buttons
         foodButton = findViewById(R.id.foodButton);
