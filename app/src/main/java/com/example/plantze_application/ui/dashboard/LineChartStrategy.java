825ecae3a9c9bcae43415d6167d35b40fcce2d01
charlesgchen <charlesg.chen@mail.utoronto.ca>
2024-12-03 12:57:05 -0500

    feat:added integration with database for ecogauge, added week, month, year options to display co2 emissions over the past range.

---
+++ app/src/main/java/com/example/plantze_application/ui/dashboard/LineChartStrategy.java
@@ -0,0 +1,12 @@
+package com.example.plantze_application.ui.dashboard;
+
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.LineChart;
+
+/*
+public class LineChartStrategy implements ChartStrategy{
+    public void setChart(Chart){
+
+    }
+}
+*/
